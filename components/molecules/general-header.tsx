1dec7f4ed608008c4dcc990561286a2163141d6d
Williams Onuaguluchi <jahbyte@Williamss-MacBook-Air.local>
2026-03-02 01:29:17 +0100

    Feature/Header_Fix_1

--- components/molecules/general-header.tsx
+++ components/molecules/general-header.tsx
@@ -19,10 +19,10 @@ export default function GeneralHeader() {
   }
   return (
     <header className="sticky top-0 z-50 left-0 right-0 flex flex-row bg-linear-to-r items-center justify-between px-2 py-2 from-[#d9480f] via-[#1c7ed6] to-orange-500 text-white">
-      <Link href="/" className="font-bold text-lg md:text-3xl">
+      <Link href="/" className="font-bold text-sm md:text-3xl">
         LUXESTATE
       </Link>
-      <div className="flex flex-row items-center gap-2 md:gap-4 text-sm md:text-lg">
+      <div className="flex flex-row items-center gap-2 md:gap-4 text-[10px] md:text-lg">
         <Link href="/">Home</Link>
         <Link href="/about">About Us</Link>
         <Link href="/properties">Properties</Link>
